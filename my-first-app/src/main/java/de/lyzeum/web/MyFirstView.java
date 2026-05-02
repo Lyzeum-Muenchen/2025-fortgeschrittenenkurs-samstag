f67460e9e6df1f866215a71ae30934b2ca3bc7c3
Maximilian Geitner <maximilian.geitner@outlook.de>
2026-05-02 13:26:52 +0200

    Fortschritt 02.05.2026
    - Palindrome Checker

--- my-first-app/src/main/java/de/lyzeum/web/MyFirstView.java
+++ my-first-app/src/main/java/de/lyzeum/web/MyFirstView.java
@@ -1,7 +1,13 @@
 package de.lyzeum.web;
 
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -9,8 +15,59 @@ import com.vaadin.flow.router.Route;
 @PageTitle("My First View")
 public class MyFirstView extends VerticalLayout {
 
+	final TextField txtInput;
+	final Paragraph outputParagraph;
+
 	public MyFirstView() {
 		H1 firstTitle = new H1("Palindrome Checker");
-		add(firstTitle);
+
+		txtInput = new TextField();
+		txtInput.setPlaceholder("Please enter a Palindrome");
+		txtInput.setMinWidth("20em");
+		txtInput.setValueChangeMode(ValueChangeMode.EAGER);
+		txtInput.addKeyPressListener(event -> {
+			if (event.getKey().equals(Key.ENTER)) {
+				checkPalindrome();
+			}
+		});
+
+		Button button = new Button("Check Input");
+		button.addClickListener(
+			event -> checkPalindrome()
+		);
+
+		outputParagraph = new Paragraph();
+
+		add(
+			firstTitle,
+			new HorizontalLayout(txtInput, button),
+			outputParagraph
+		);
+	}
+
+	private void checkPalindrome() {
+		if (isPalindrome(txtInput.getValue())) {
+			outputParagraph.setText(
+					txtInput.getValue() + " is a Palindrome!"
+			);
+		} else {
+			outputParagraph.setText(
+					txtInput.getValue() + " is not a Palindrome!"
+			);
+		}
+	}
+
+
+	public boolean isPalindrome(String input) {
+		if (input.isBlank()) {
+			return false;
+		}
+		String lowercaseInput = input.toLowerCase();
+		for (int i = 0; i <= input.length() / 2; i++) {
+			if(lowercaseInput.charAt(i) != lowercaseInput.charAt(input.length() - 1 - i)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
